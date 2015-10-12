66f2b8fb0e1a320ef778627b1b30ce192aac3a72
ruxandrafed <ruxandra.fediuc@gmail.com>
2015-10-12 12:43:53 -0700

    Exercise 5 done

--- 05_filtered_ls.js
+++ 05_filtered_ls.js
@@ -1,15 +1,31 @@
 var fs = require('fs');
 var path = require('path');
 
-var directoryPath = process.argv[2];
-var fileExtension = process.argv[3];
-var regex = new RegExp("." + fileExtension + "$", "g");
+var regex = new RegExp('\.' + process.argv[3] + '$');
 
-fs.readdir(directoryPath, function (err, files) {
-  if (err) throw err;
-  for (i=0; i< files.length; i++) {
-    if (regex.test(files[i])) {
-      console.log(files[i]+"\n");
-    }
-  }
-});
+var matching = function(filename) {
+  return regex.test(filename)
+};
+
+var printFiltered = function (err, arr) {
+  arr.sort()
+    .filter(matching)
+    .forEach(function (filename) {
+      console.log(filename);
+    });
+};
+
+fs.readdir(process.argv[2], printFiltered);
+
+
+// var fs = require('fs')
+// var path = require('path')
+
+// fs.readdir(process.argv[2], function (err, list) {
+//   list.forEach(function (file) {
+//     if (path.extname(file) === '.' + process.argv[3])
+//       console.log(file)
+//   })
+// })
+
+
