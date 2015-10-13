c989a368218378d1241bea8c2aecea0b9597b61c
ruxandrafed <ruxandra.fediuc@gmail.com>
2015-10-12 21:23:25 -0700

    Exercise 10 done

---
+++ 10_time_server.js
@@ -0,0 +1,31 @@
+var net = require('net'),
+    strftime = require('strftime'),
+    port = process.argv[2];
+
+var server = net.createServer(function createServerCallback(socket) {
+  return socket.end(strftime('%Y-%m-%d %H:%M'));
+});
+
+server.listen(port);
+
+
+// var net = require('net')
+
+// function zeroFill(i) {
+//   return (i < 10 ? '0' : '') + i
+// }
+
+// function now () {
+//   var d = new Date()
+//   return d.getFullYear() + '-'
+//     + zeroFill(d.getMonth() + 1) + '-'
+//     + zeroFill(d.getDate()) + ' '
+//     + zeroFill(d.getHours()) + ':'
+//     + zeroFill(d.getMinutes())
+// }
+
+// var server = net.createServer(function (socket) {
+//   socket.end(now() + '\n')
+// })
+
+// server.listen(Number(process.argv[2]))
