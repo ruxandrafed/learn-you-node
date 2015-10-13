c3912c9790de08cd87d9979816b4c807168799ae
ruxandrafed <ruxandra.fediuc@gmail.com>
2015-10-12 20:58:18 -0700

    Exercise 9 done

--- 09_juggling_async.js
+++ 09_juggling_async.js
@@ -1,30 +1,66 @@
-var http = require('http');
-var url = process.argv[2];
+var http = require('http'),
+    urls = process.argv.slice(2),
+    results = {},
+    count = 0;
 
-var collector = [];
 
-http.get(url, function httpGetCallback(response) {
-  response.setEncoding('utf8');
-  response.on('error', console.error);
-  response.on('data', function(input) {
-    collector.push(input);
+var print = function (results) {
+
+  for (var i = 0; i < 3; ++i) {
+    console.log(results[i]);
+  }
+};
+
+
+var task = function (i) {
+
+  http.get(urls[i], function (response) {
+
+    var data = '';
+    response.setEncoding('utf8');
+    response.on('error', console.error);
+    response.on('data', function (d) {
+      data += d;
+    });
+    response.on('end', function () {
+      results[i] = data;
+      count += 1;
+      if (count === 3) {
+        print(results);
+      }
+    });
   });
-  response.on('end', function() {
-    console.log(collector.join("").length);
-    console.log(collector.join(""));
-  });
-});
+};
 
+
+for (var i = 0; i < 3; ++i) {
+  task(i);
+}
 
 // var http = require('http')
 // var bl = require('bl')
+// var results = []
+// var count = 0
 
-// http.get(process.argv[2], function (response) {
-//   response.pipe(bl(function (err, data) {
-//     if (err)
-//       return console.error(err)
-//     data = data.toString()
-//     console.log(data.length)
-//     console.log(data)
-//   }))
-// })
+// function printResults () {
+//   for (var i = 0; i < 3; i++)
+//     console.log(results[i])
+// }
+
+// function httpGet (index) {
+//   http.get(process.argv[2 + index], function (response) {
+//     response.pipe(bl(function (err, data) {
+//       if (err)
+//         return console.error(err)
+
+//       results[index] = data.toString()
+//       count++
+
+//       if (count == 3)
+//         printResults()
+//     }))
+//   })
+// }
+
+// for (var i = 0; i < 3; i++)
+//   httpGet(i)
