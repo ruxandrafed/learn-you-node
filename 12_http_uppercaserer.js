b526530a22fcf4faf9fc53c3a7936fb4cfa05c41
ruxandrafed <ruxandra.fediuc@gmail.com>
2015-10-12 21:57:54 -0700

    Exercise 12 done

--- 12_http_uppercaserer.js
+++ 12_http_uppercaserer.js
@@ -1,9 +1,30 @@
-var fs = require('fs'),
-    http = require('http'),
-    port = process.argv[2];
+var http = require('http'),
+    port = process.argv[2],
+    map = require('through2-map');
+
+var upcase = function (chunk) {
+  return chunk.toString().toUpperCase();
+};
 
 var server = http.createServer(function createHttpServerCallback(request, response) {
-
+  if (request.method === 'POST') {
+    return request.pipe(map(upcase)).pipe(response);
+  }
 });
 
-server.listen(Number(port);
+server.listen(port);
+
+
+// var http = require('http')
+// var map = require('through2-map')
+
+// var server = http.createServer(function (req, res) {
+//   if (req.method != 'POST')
+//     return res.end('send me a POST\n')
+
+//   req.pipe(map(function (chunk) {
+//     return chunk.toString().toUpperCase()
+//   })).pipe(res)
+// })
+
+// server.listen(Number(process.argv[2]))
